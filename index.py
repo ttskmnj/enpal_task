3bedd0daab19fac5e9beea72626e8af0703448d9
Tatsuki Monji <ttskmnj@ttskmnj-macmini.fritz.box>
2023-12-13 12:02:38 +0100

    add task_e, edit __calc_distance , fix typo in private method name

--- index.py
+++ index.py
@@ -30,10 +30,8 @@ class Enpal:
 
       visit_index += self.quota[i]
 
-  def _calc_distance(self, eng_idx):
+  def __calc_distance(self, eng_idx, sequence_of_visits):
     total_distance = 0
-    
-    sequence_of_visits = self.sequence_of_visits[eng_idx]
 
     cur_loc = eng_idx
 
@@ -55,9 +53,9 @@ class Enpal:
       self.task_b()
 
     for i in range(self.engineers):
-      self.total_distance.append(self._calc_distance(i))
+      self.total_distance.append(self.__calc_distance(i, self.sequence_of_visits[i]))
 
-  def _change_sequence(self, sequence_of_visits, route_change):
+  def __change_sequence(self, sequence_of_visits, route_change):
       new_sequence_of_visits = []
 
       origin, dest = route_change
@@ -76,21 +74,48 @@ class Enpal:
     if len(self.sequence_of_visits) == 0:
       self.task_b()
 
-    for i in range(self.engineers):
-      self.sequence_of_visits[i] = \
-        self._change_sequence(
-          self.sequence_of_visits[i],
-          route_change[i]
+    for k, v in enumerate(self.sequence_of_visits):
+      self.sequence_of_visits[k] = \
+        self.__change_sequence(
+          v,
+          route_change[k]
         )
 
+  def task_e(self):
+    if len(self.distance) == 0:
+      self.task_a()
+
+    if len(self.sequence_of_visits) == 0:
+      self.task_b()
+
+    for k, v in enumerate(self.sequence_of_visits):
+      for i in range(200):
+        # generate list of index of sequence_of_visits and randomly choose origin and destination
+        indexes = list(range(len(v)))
+        origin = indexes.pop(random.randint(0, len(indexes) - 1))
+        dest = indexes.pop(random.randint(0, len(indexes) - 1))
+      
+        changed_sequence = \
+          self.__change_sequence(
+            v,
+            [origin, dest]
+          )
+        
+        new_distance = self.__calc_distance(k, changed_sequence)
+
+        # update sequence of visit if distance is shorter
+        if new_distance < self.total_distance[k]:
+          self.sequence_of_visits[k] = changed_sequence
+          self.total_distance[k] = new_distance
+          break
 
 ################################################################
 # create Enpal class instance with following setting
 # engineer: 2
 # customer: 8
-# engineer_0 visit 3 customers, engineer_1 visit 5 customers
+# engineer_0 visit 5 customers, engineer_1 visit 3 customers
 ################################################################
-enpal = Enpal(2, 8, [3, 5])
+enpal = Enpal(2, 8, [5, 3])
 
 ###########################
 # generate distance matrix
@@ -121,12 +146,22 @@ print(enpal.total_distance)
 
 ################################
 # change the sequence of visits
-# engineer_0: switch 2nd visit and 3rd visit
-# engineer_1: switch 5th visit and 1st visit
+# engineer_0: switch 5th visit and 2nd visit
+# engineer_1: switch 2nd visit and 3rd visit
 ################################
-enpal.task_d([[1, 2], [4, 0]])
+enpal.task_d([[4, 1], [1, 2]])
 
 print("\n# changed sequence of visits")
-print("# engineer_0: switch 2nd visit and 3rd visit")
-print("# engineer_1: switch 5th visit and 1st visit")
+print("# engineer_0: switch 5th visit and 2nd visit")
+print("# engineer_1: switch 2nd visit and 3rd visit")
 print(enpal.sequence_of_visits)
+
+##################################
+# change the sequence of visits
+# only if distance become shorter
+##################################
+enpal.task_e()
+print("\n# shorter sequence of visits")
+print(enpal.sequence_of_visits)
+print("# shorter total_distance")
+print(enpal.total_distance)
